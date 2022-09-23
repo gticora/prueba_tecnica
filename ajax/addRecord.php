911a7635d333990ab42ad0d5d3cbfb70f408ae1c
Gustavo Ticora <gticora@sinergiaonline.com>
2022-09-22 19:29:11 -0500

    se agregan valdiacion javascript en cuanto a los errores por tiempoen transacciones

--- ajax/addRecord.php
+++ ajax/addRecord.php
@@ -13,21 +13,21 @@ if(isset($_POST['nombre'])){
     $area=$_POST['area'];
     $descripcion=$_POST['descripcion'];
     $boletin=$_POST['boletin'];
-    $roles=$_POST['roles'];
+    //$roles=$_POST['roles'];
 
 	if($user_id!=''){
 			$query2 = "UPDATE empleado SET nombre='$nombre', email='$email', sexo='$sexo',  area_id='$area', descripcion = '$descripcion', boletin = '$boletin' WHERE id = '$user_id'";
 			if (!$result2 = mysqli_query($con, $query2)) {
 				exit(mysqli_error($con));
 			}
-			echo "1 Record Added!";
+			echo "Se actualizo el rgistro correctamente!";
 		
 	}else{
 		$query4 = "INSERT INTO empleado(nombre, email, sexo, area_id, descripcion, boletin) VALUES('$nombre', '$email', '$sexo', '$area', '$descripcion', '$boletin')";
 		if (!$result4 = mysqli_query($con, $query4)) {
 			exit(mysqli_error($con));
 		}
-		echo "1 Record Added!";
+		echo "Se guardo el registro correctamente!";
 	}
 }
 ?>
--- js/script.js
+++ js/script.js
@@ -11,24 +11,15 @@ function addRecord() {
     var boletin = $("#boletin").val();
     var roles = $("#roles").val();
 
-    // Add record
-    $.post("ajax/addRecord.php", {
-        nombre: nombre,
-        email: email,
-        sexo: sexo,
-        area: area,
-        descripcion: descripcion,
-        boletin: boletin,
-        roles: roles,
-        id: id
-    }, function(data, status) {
-        console.log(data);
-        // close the popup
+    $.ajax({
+        type: 'POST', // Envío con método POST
+        url: 'ajax/addRecord.php', // Fichero destino (el PHP que trata los datos)
+        data: { nombre: nombre, email: email, sexo: sexo, area: area, descripcion: descripcion, boletin: boletin, roles: roles, id: id } // Datos que se envían
+    }).done(function(msg) { // Función que se ejecuta si todo ha ido bien
+        alert(msg);
         $("#add_new_record_modal").modal("hide");
-
         // read records again
         readRecords();
-
         // clear fields from the popup
         $("#nombre").val("");
         $("#email").val("");
@@ -37,13 +28,53 @@ function addRecord() {
         $("#descripcion").val("");
         $("#boletin").val("");
         $("#roles").val("");
+
+    }).fail(function(jqXHR, textStatus, errorThrown) { // Función que se ejecuta si algo ha ido mal
+        // Mostramos en consola el mensaje con el error que se ha producido
+        if (jqXHR.status === 0) {
+            alert('Not connect: Verify Network.');
+        } else if (jqXHR.status == 404) {
+            alert('Requested page not found [404]');
+        } else if (jqXHR.status == 500) {
+            alert('Internal Server Error [500].');
+        } else if (textStatus === 'parsererror') {
+            alert('Requested JSON parse failed.');
+        } else if (textStatus === 'timeout') {
+            alert('Time out error.');
+        } else if (textStatus === 'abort') {
+            alert('Ajax request aborted.');
+        } else {
+            alert('Uncaught Error: ' + jqXHR.responseText);
+        }
     });
 }
 
 // READ records
 function readRecords() {
-    $.get("ajax/readRecord.php", {}, function(data, status) {
-        $("#records_content").html(data);
+
+    $.ajax({
+        type: 'GET', // Envío con método POST
+        url: 'ajax/readRecord.php', // Fichero destino (el PHP que trata los datos)
+    }).done(function(msg) { // Función que se ejecuta si todo ha ido bien
+        $("#records_content").html(msg);
+
+    }).fail(function(jqXHR, textStatus, errorThrown) { // Función que se ejecuta si algo ha ido mal
+        // Mostramos en consola el mensaje con el error que se ha producido
+        if (jqXHR.status === 0) {
+            alert('Not connect: Verify Network.');
+        } else if (jqXHR.status == 404) {
+            alert('Requested page not found [404]');
+        } else if (jqXHR.status == 500) {
+            alert('Internal Server Error [500].');
+        } else if (textStatus === 'parsererror') {
+            alert('Requested JSON parse failed.');
+        } else if (textStatus === 'timeout') {
+            alert('Time out error.');
+        } else if (textStatus === 'abort') {
+            alert('Ajax request aborted.');
+        } else {
+            alert('Uncaught Error: ' + jqXHR.responseText);
+        }
     });
 }
 
@@ -51,43 +82,81 @@ function readRecords() {
 function DeleteUser(id) {
     var conf = confirm("¿Está seguro, realmente desea eliminar el registro?");
     if (conf == true) {
-        $.post("ajax/deleteUser.php", {
-                id: id
-            },
-            function(data, status) {
-                // reload Users by using readRecords();
-                readRecords();
+        $.ajax({
+            type: 'POST', // Envío con método POST
+            url: 'ajax/deleteUser.php', // Fichero destino (el PHP que trata los datos)
+            data: { id: id } // Datos que se envían
+        }).done(function(msg) { // Función que se ejecuta si todo ha ido bien
+            readRecords();
+
+        }).fail(function(jqXHR, textStatus, errorThrown) { // Función que se ejecuta si algo ha ido mal
+            // Mostramos en consola el mensaje con el error que se ha producido
+            if (jqXHR.status === 0) {
+                alert('Not connect: Verify Network.');
+            } else if (jqXHR.status == 404) {
+                alert('Requested page not found [404]');
+            } else if (jqXHR.status == 500) {
+                alert('Internal Server Error [500].');
+            } else if (textStatus === 'parsererror') {
+                alert('Requested JSON parse failed.');
+            } else if (textStatus === 'timeout') {
+                alert('Time out error.');
+            } else if (textStatus === 'abort') {
+                alert('Ajax request aborted.');
+            } else {
+                alert('Uncaught Error: ' + jqXHR.responseText);
             }
-        );
+        });
     }
+
 }
 
 function GetUserDetails(id) {
     // Add User ID to the hidden field for furture usage
 
     $("#hidden_user_id").val(id);
-    $.post("ajax/readUserDetails.php", {
-            id: id
-        },
-        function(data, status) {
-            // PARSE json data
-            var user = JSON.parse(data);
-            // Assing existing values to the modal popup fields
-            $("#nombre").val(user.nombre);
-            $("#email").val(user.email);
-            $("#area").val(user.area_id);
-            $("#boletin").val(user.boletin);
-            //$("#descripcion").val(user.descripcion);
 
-            if (user.sexo == 'M') $("#M").attr("checked", true);
-            else if (user.sexo == 'F') $("#F").attr("checked", true);
+    $.ajax({
+        type: 'POST', // Envío con método POST
+        url: 'ajax/readUserDetails.php', // Fichero destino (el PHP que trata los datos)
+        data: { id: id } // Datos que se envían
+    }).done(function(msg) { // Función que se ejecuta si todo ha ido bien
+        // PARSE json data
+        var user = JSON.parse(msg);
+        // Assing existing values to the modal popup fields
+        $("#nombre").val(user.nombre);
+        $("#email").val(user.email);
+        $("#area").val(user.area_id);
+        $("#boletin").val(user.boletin);
+        $("#descripcion").val(user.descripcion);
 
-            if (user.boletin == '1') $("#boletin").prop("checked", true);
-            else if (user.boletin == '2') $("#boletin").prop("checked", false);
+        if (user.sexo == 'M') $("#M").attr("checked", true);
+        else if (user.sexo == 'F') $("#F").attr("checked", true);
 
-            $("#rol ").prop("checked", true);
+        if (user.boletin == '1') $("#boletin").prop("checked", true);
+        else if (user.boletin == '2') $("#boletin").prop("checked", false);
+
+        $("#rol ").prop("checked", true);
+
+    }).fail(function(jqXHR, textStatus, errorThrown) { // Función que se ejecuta si algo ha ido mal
+        // Mostramos en consola el mensaje con el error que se ha producido
+        if (jqXHR.status === 0) {
+            alert('Not connect: Verify Network.');
+        } else if (jqXHR.status == 404) {
+            alert('Requested page not found [404]');
+        } else if (jqXHR.status == 500) {
+            alert('Internal Server Error [500].');
+        } else if (textStatus === 'parsererror') {
+            alert('Requested JSON parse failed.');
+        } else if (textStatus === 'timeout') {
+            alert('Time out error.');
+        } else if (textStatus === 'abort') {
+            alert('Ajax request aborted.');
+        } else {
+            alert('Uncaught Error: ' + jqXHR.responseText);
         }
-    );
+    });
+
     // Open modal popup
     $("#add_new_record_modal").modal("show");
 }
